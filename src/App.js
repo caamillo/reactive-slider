b2f5d8e158b7851584815f62bbc36f9d587cae91
caamillo <diaskygotah@gmail.com>
2022-09-09 16:34:50 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import './css/slider.css'
 function App() {
 
   return (
-    <div className="App h-screen bg-[#121212]">
+    <div className="App h-screen">
       <div className='flex items-center justify-center h-full'>
-        <div className='bg-[#0f0f0f] aspect-video w-[50%] flex justify-center items-center rounded-lg'>
+        <div className='slider-container bg-[#0f0f0f] aspect-video w-[50%] flex justify-center items-center rounded-lg'>
           <input id="typeinp" type="range" min="0" className='slider' max="100" defaultValue="0" step="1" onChange={ (e) => console.log(e.target.value) }/>
         </div>
       </div>
