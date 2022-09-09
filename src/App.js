4e93d197f84716c96c6db466bb376a2ffeb9abf6
caamillo <diaskygotah@gmail.com>
2022-09-09 10:42:19 +0200

    get cursor coordinates relative to slider-bar

--- src/App.js
+++ src/App.js
@@ -19,15 +19,37 @@ function App() {
     })
   }, [])
 
+  const getRelativeCoordinates = (event, referenceElement) => {
+
+    const position = {
+      x: event.pageX,
+      y: event.pageY
+    };
+  
+    const offset = {
+      left: referenceElement.offsetLeft,
+      top: referenceElement.offsetTop
+    };
+  
+    let reference = referenceElement.offsetParent;
+  
+    return { 
+      x: position.x - offset.left,
+      y: position.y - offset.top,
+    }; 
+  
+  }
+
   useEffect(() => {
     const root = document.getElementById('root')
     const sliderBtn = document.getElementById('slider-button')
 
-    const endSliding = () => {
+    const endSliding = (e) => {
       if (isSliding) {
         console.log('end sliding')
         sliderBtn.style.backgroundColor = '#a26ed2'
         setIsSliding(false)
+        console.log(getRelativeCoordinates(e, document.getElementById('slider-bar')))
       }
     }
 
@@ -42,8 +64,8 @@ function App() {
     <div className="App h-screen bg-[#121212]">
       <div className='flex items-center justify-center h-full'>
         <div className='bg-[#0f0f0f] aspect-video w-[50%] flex justify-center rounded-lg'>
-          <div id='slider-bar' className='bg-[#454545] h-full m-5 w-[15px] rounded-md flex flex-col items-center'>
-            <div id='slider-button' className='aspect-square w-[40px] bg-[#a26ed2] flex justify-center items-center rounded-[50%] cursor-default'>
+          <div id='slider-bar' className='bg-[#454545] h-full m-5 w-[15px] rounded-md flex flex-col items-center justify-end'>
+            <div id='slider-button' className='aspect-square w-[40px] bg-[#a26ed2] flex justify-center items-center rounded-[50%] cursor-default absolute top-[83%]'>
               
             </div>
           </div>
