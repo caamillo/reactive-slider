4679059c356613ddac012511c5db960592c59b3d
caamillo <diaskygotah@gmail.com>
2022-09-09 09:31:26 +0200

    wrapping slider

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './tailwind/output.css'
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div className="App h-screen bg-[#121212]">
+      <div className='flex items-center justify-center h-full'>
+        <div className='bg-black aspect-video w-[80%] flex justify-center'>
+          <div id='slider' className='bg-[#ff0000] h-full m-5 w-[20px]'>
+            test
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
